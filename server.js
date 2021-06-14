26ed6594d08e49815c341add1101d3062c5858e4
Dmytro Omelchuk <dmytro.omelchuk@eleks.com>
2021-06-14 10:21:19 +0300

    add

---
+++ server.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 5000
+
+const server = https.createServer((req, res) => {
+  if (req.url === '/statistic') {
+    console.log('statistic ', Date.now());
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.end('Hellow world');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
